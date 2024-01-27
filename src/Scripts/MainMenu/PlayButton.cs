6af29507c2ca4d6e74018e521f51966ee6319628
Alexandre Billonnet <alexandre.billonnet@etu.unilim.fr>
2024-01-27 14:16:26 +0100

    addi titlescreen picture and correct play button

--- src/Scripts/MainMenu/PlayButton.cs
+++ src/Scripts/MainMenu/PlayButton.cs
@@ -6,11 +6,11 @@ public partial class PlayButton : Button
 {
     private void _on_start_button_pressed()
     {
-        if (GetTree().ChangeSceneToFile("res://FirstScene.tscn") != Error.Ok)
+        if (GetTree().ChangeSceneToFile("res://PlayingScene/FirstScene.tscn") != Error.Ok)
             GD.PrintErr("Failed to change scene to FirstScene.tscn");
         else
         {
-            GetTree().ChangeSceneToFile("res://FirstScene.tscn");
+            GetTree().ChangeSceneToFile("res://PlayingScene/FirstScene.tscn");
         }
     }
 }
