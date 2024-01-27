6ef99233ad6a9aa4748621c9eadcd58d5bd2fa78
Logan Ferreira <logan.ferreira@etu.unilim.fr>
2024-01-27 17:12:47 +0100

    Add Movement for Koopa

--- src/Scripts/PlayingScene/FirstLevel.cs
+++ src/Scripts/PlayingScene/FirstLevel.cs
@@ -3,14 +3,14 @@ using System;
 
 public partial class FirstLevel : Control
 {
-    private static Camera2D _camera;
-    public override void _Ready()
-    {
-	    base._Ready();
-	    
-    }
+	private static Camera2D _camera;
+	public override void _Ready()
+	{
+		base._Ready();
+		
+	}
 
-    public void _on_tree_entered()
+	public void _on_tree_entered()
 	{
 		_camera = GetNode<Camera2D>("Player/Camera2D");
 		_camera.Enabled = true;
