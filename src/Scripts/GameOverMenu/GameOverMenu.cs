47d295fb89bf2fde79b23b732b62f80d07038df1
Alexandre Billonnet <alexandre.billonnet@etu.unilim.fr>
2024-01-28 12:12:21 +0100

    add gameover label manager

---
+++ src/Scripts/GameOverMenu/GameOverMenu.cs
@@ -0,0 +1,30 @@
+using Godot;
+
+namespace SuperMarigolo.Scripts.GameOverMenu;
+
+public partial class GameOverMenu : Node
+{
+	public static int _index;
+	private Label _label;
+	private readonly string[] _text =
+	{
+		"Bah alors on a pris la grosse tete ?",
+		"text2",
+		"text3",
+		"text4",
+	};
+	public override void _Ready()
+	{
+		base._Ready();
+		_label = GetNode<Label>("CanvasLayer/Control/CenterContainer/PanelContainer/VBoxContainer/Text");
+		Reload();
+	}
+	private void Reload()
+	{
+		_label.Text = _text[_index];
+	}
+	public static void SetIndex(int index)
+	{
+		_index = index;
+	}
+}
--- src/Scripts/PatternState/GiantState.cs
+++ src/Scripts/PatternState/GiantState.cs
@@ -1,17 +1,13 @@
-using Godot;
 
-namespace SuperMarigolo.Scripts.PatternState;
+using SuperMarigolo.Scripts.GameOverMenu;
 
 public class GiantState: State
 {
-    
-
-    
     public State doState(Player player)
     {
+        GameOverMenu.SetIndex(0);
         player.changeSpeed(150f);
         player.changeGravity(2000f);
         return player.GiantState;
     }
-    
 }
