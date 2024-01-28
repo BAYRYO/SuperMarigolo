84f8f7f9084f5b83ec535233997ae04e3621a4c7
Logan Ferreira <logan.ferreira@etu.unilim.fr>
2024-01-28 12:58:37 +0100

    Add Piranha and Goomba sprites and movement

---
+++ src/Scripts/Piranha/Piranha.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class Piranha : Node2D
+{
+	private AnimationPlayer _characterBody;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_characterBody = GetNode<AnimationPlayer>("AnimationPlayer");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		_characterBody.Play("Piranha");
+	}
+}
